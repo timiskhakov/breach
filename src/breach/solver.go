6b0b438fbb444c88bc67ca2c72e8735f8902a653
Timur Iskhakov <timiskhakov@gmail.com>
2022-11-06 12:40:03 +0200

    Make stack's methods private

--- src/breach/solver.go
+++ src/breach/solver.go
@@ -6,12 +6,12 @@ func Solve(matrix [][]byte, seq []byte) []Point {
 			if matrix[i][j] == seq[0] {
 				s := stack{}
 				dfs(matrix, seq, &s, Point{i, j}, i != 0)
-				if s.Len() == len(seq) {
+				if s.len() == len(seq) {
 					if i != 0 {
-						return append([]Point{{0, j}}, s.Slice()...)
+						return append([]Point{{0, j}}, s.slice()...)
 					}
 
-					return s.Slice()
+					return s.slice()
 				}
 			}
 		}
@@ -21,28 +21,28 @@ func Solve(matrix [][]byte, seq []byte) []Point {
 }
 
 func dfs(matrix [][]byte, seq []byte, s *stack, p Point, hrz bool) {
-	s.Push(p)
-	if s.Len() == len(seq) {
+	s.push(p)
+	if s.len() == len(seq) {
 		return
 	}
 
 	if hrz {
 		for i := 0; i < len(matrix[0]); i++ {
 			candidate := Point{p.x, i}
-			if s.Len() < len(seq) && matrix[p.x][i] == seq[s.Len()] && !s.Exists(candidate) {
+			if s.len() < len(seq) && matrix[p.x][i] == seq[s.len()] && !s.exists(candidate) {
 				dfs(matrix, seq, s, candidate, false)
 			}
 		}
 	} else {
 		for i := 0; i < len(matrix); i++ {
 			candidate := Point{i, p.y}
-			if s.Len() < len(seq) && matrix[i][p.y] == seq[s.Len()] && !s.Exists(candidate) {
+			if s.len() < len(seq) && matrix[i][p.y] == seq[s.len()] && !s.exists(candidate) {
 				dfs(matrix, seq, s, candidate, true)
 			}
 		}
 	}
 
-	if s.Len() < len(seq) {
-		s.Pop()
+	if s.len() < len(seq) {
+		s.pop()
 	}
 }
--- src/breach/stack.go
+++ src/breach/stack.go
@@ -4,11 +4,11 @@ type stack struct {
 	storage []Point
 }
 
-func (s *stack) Push(p Point) {
+func (s *stack) push(p Point) {
 	s.storage = append(s.storage, p)
 }
 
-func (s *stack) Pop() {
+func (s *stack) pop() {
 	if len(s.storage) == 0 {
 		return
 	}
@@ -16,20 +16,20 @@ func (s *stack) Pop() {
 	s.storage = s.storage[:len(s.storage)-1]
 }
 
-func (s *stack) Len() int {
+func (s *stack) len() int {
 	return len(s.storage)
 }
 
-func (s *stack) Exists(p Point) bool {
+func (s *stack) exists(p Point) bool {
 	for _, v := range s.storage {
 		if v == p {
 			return true
 		}
 	}
-	
+
 	return false
 }
 
-func (s *stack) Slice() []Point {
+func (s *stack) slice() []Point {
 	return s.storage
 }
